e5f914c85721468fc2d2b89e0e6a26f222c79dd8
mdshazzad <sazzad15-2503@diu.edu.bd>
2022-11-06 16:02:03 +0600

    Initial commit

---
+++ bin/dart1.dart
@@ -0,0 +1,9 @@
+import 'package:dart1/dart1.dart' as dart1;
+
+void main() {
+  int x= 10;
+  double y= 10.5;
+  String name= 'seip';
+  bool status = false;
+  print('the mul of $x and $y is ${x*y}');
+}
